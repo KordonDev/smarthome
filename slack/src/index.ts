3072ffc2f61721f3c8104acf4af09650fb72035b
Arne Maier <Arne_Maier@gmx.de>
2020-11-02 20:27:24 +0100

    Logic to set and reset fire run

--- slack/src/index.ts
+++ slack/src/index.ts
@@ -1,1 +1,47 @@
-console.log('hello 2')
+import { setProfile, getProfile, Status } from './slack'
+import mqtt from 'mqtt'
+
+const host = process.env.HA_MQTT_HOST
+const user = process.env.HA_MQTT_USER
+const password = process.env.HA_MQTT_PASSWORD
+
+if (host === undefined || user === undefined || password === undefined) {
+  console.log('Missing configuration parameter')
+}
+
+const client = mqtt.connect({
+  host,
+  port: '1883',
+  username: user,
+  password,
+  protocolId: 'MQTT',
+  protocolVerison: 5,
+})
+
+let oldStatus: Status = {}
+let fireRunActive = false
+
+client.on('error', function (topic: string) {
+  console.log('errro', topic)
+})
+
+client.on('disconnect', () => {
+  console.log('disconnect')
+})
+
+client.on('connect', function () {
+  client.subscribe(['fire_run', 'arne_entering_home'], { qos: 0 })
+
+  client.on('message', async function (topic: string, message: object) {
+    console.log(topic)
+    if (topic === 'fire_run') {
+      oldStatus = await getProfile()
+      fireRunActive = true
+      setProfile('Feuerwehreinsatz', ':fire_engine:')
+    }
+
+    if (topic === 'arne_entering_home' && fireRunActive) {
+      setProfile(oldStatus.statusText, oldStatus.statusEmoji)
+    }
+  })
+})
